cb7c323e1fa4031a5b961bd2393056886c7103c0
SeamusHerrod <85411251+SeamusHerrod@users.noreply.github.com>
2023-02-13 14:14:29 -0600

    finished CProgram2

--- CProgram2/CProgram2.c
+++ CProgram2/CProgram2.c
@@ -27,15 +27,44 @@ int main()
 	  TD + 1-pt FG 7 pt. 
 	*/
 
-	int num_TD, num_FG, num_safe, num_TD_conv, num_TD_FG, current_total;
+	//int num_TD, num_FG, num_safe, num_TD_conv, num_TD_FG, current_total;
 
 	for ( int TD_2pt = 0; TD_2pt <= score / 8; TD_2pt++ )
 	{
-		current_total = score - ( TD_2pt * 8 );
-		for ( int TD_1pt = 0; TD_1pt <= score / 7; TD_1pt++ )
+		int num_TD_conv = score;
+		num_TD_conv -= ( TD_2pt * 8 );
+
+		for ( int TD_1pt = 0; TD_1pt <= num_TD_conv / 7; TD_1pt++ )
 		{
-	
+			int num_TD_FG = num_TD_conv - ( TD_1pt * 7 ); 
+
+			for ( int td = 0; td <= num_TD_FG / 6; td++ )
+			{
+				int num_TD = num_TD_FG - ( td * 6 );
+
+				for ( int fg = 0; fg <= num_TD / 3; fg++ )
+				{
+					int num_FG = num_TD - ( fg * 3 );
+					if ( num_FG % 2 == 0 )
+					{
+						for ( int safe = 0; safe <= num_FG / 2; safe++ )
+						{
+						
+							int num_safe = num_FG - ( safe * 2 );
+							//printf("current total = %d\n", current_total);						       
+							int total = TD_2pt*8 + TD_1pt*7 + td*6 + fg*3 + safe*2;
+							//printf("total = %d\n", total);
+							//current_total += ( safe * 2 ) ;
+							if ( total == score )
+							{
+							printf("TD_2pt = %d, TD_1pt = %d, TD = %d, FG = %d, safety = %d\n", TD_2pt, TD_1pt, td, fg, safe);
+							}
+						}
+					}
+				}
+			}
 		}
+
 	}
 
 
